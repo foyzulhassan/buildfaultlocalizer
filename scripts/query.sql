3a425f66ac2fdda4365b1ce0e1ea712ff66367e8
FoyzulHassan <foyzul.hassan@gmail.com>
2018-03-17 02:15:59 -0500

    - Add Gradle Fix Data Entity Class - Fix Hibernate configuration issue - Add Necessary class for Build Fix Data store

---
+++ scripts/query.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE gradlebuildfixdata(
+	 row bigint(20) NOT NULL,
+	 gh_project_name text,	
+	 git_branch text,
+	 git_commit text,
+	 git_failintro_commit text,
+	 git_lastfail_commit text,
+	 git_fix_commit text,
+	 bl_log mediumtext,	
+	 fail_change MEDIUMTEXT,
+	 fix_change MEDIUMTEXT,	
+	 PRIMARY KEY ( row )
+);
--- src/main/java/com/build/analyzer/main/MainClass.java
+++ src/main/java/com/build/analyzer/main/MainClass.java
@@ -1,10 +1,28 @@
 package com.build.analyzer.main;
 
+import com.build.analyzer.dtaccess.DBActionExecutor;
+import com.build.analyzer.dtaccess.SessionGenerator;
+
+
+
+
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DBActionExecutor dbobj = new DBActionExecutor();
+		long rowcount = dbobj.getTotalNumberofRows();
+		
+		System.out.println(rowcount);
+		
+		cleanupResource();
 
+
+	}
+	
+	private static void cleanupResource() {
+		SessionGenerator.closeFactory();
 	}
 
 }
